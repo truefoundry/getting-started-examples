34efacadc22f3930d1efd0fe6a57c0deae86f359
Shubham Rai <shubham.rai.iitg@gmail.com>
2023-08-09 15:20:26 +0530

    Update app.py

--- docker-deploy/app.py
+++ docker-deploy/app.py
@@ -26,7 +26,7 @@ inputs = [sepal_length_input, sepal_width_input,
           petal_length_input, petal_width_input]
 
 output = gr.Number()
-print("Okay I am")
+print("Non non")
 
 gr.Interface(
     fn=model_inference,
