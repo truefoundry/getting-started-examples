0becbc76e901e492db54efe71e5969ceafa793e3
debajyoti-truefoundry <debajyoti@truefoundry.com>
2023-09-12 14:10:32 +0530

    fix: improve deploy-ml-model flow

--- deploy-ml-model/deploy.py
+++ deploy-ml-model/deploy.py
@@ -1,23 +1,42 @@
+import argparse
 import logging
-from servicefoundry import Build, PythonBuild, Service, Resources
+from servicefoundry import Build, PythonBuild, Service, Resources, Port
 
 logging.basicConfig(level=logging.INFO)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--name", required=True, type=str, help="Name of the application.")
+parser.add_argument(
+    "--workspace_fqn",
+    required=True,
+    type=str,
+    help="FQN of the workspace where application will be deployed.",
+)
+parser.add_argument(
+    "--host",
+    required=True,
+    type=str,
+    help="Host where the application will be available for access. Ex:- my-app.my-org.com",
+)
+args = parser.parse_args()
 
 image = Build(
     build_spec=PythonBuild(
         command="uvicorn app:app --port 8000 --host 0.0.0.0",
-        requirements_path="requirements.txt"
+        requirements_path="requirements.txt",
     )
 )
 
 service = Service(
-    name="ml-deploy",
+    name=args.name,
     image=image,
-    ports=[{"port": 8000}],
-    resources=Resources(memory_limit=1500, memory_request=1000),
-    env={
-        "UVICORN_WEB_CONCURRENCY": "1",
-        "ENVIRONMENT": "dev"
-    }
+    ports=[Port(port=8000, host=args.host)],
+    resources=Resources(
+        cpu_request=0.1,
+        cpu_limit=0.1,
+        memory_request=500,
+        memory_limit=500,
+    ),
+    env={"UVICORN_WEB_CONCURRENCY": "1", "ENVIRONMENT": "dev"},
 )
-service.deploy(workspace_fqn="YOUR_WORKSPACE_FQN")
+service.deploy(workspace_fqn=args.workspace_fqn)
