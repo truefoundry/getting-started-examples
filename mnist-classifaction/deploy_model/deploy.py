9b384409c1d2e1c489f362f9c8ea7d25efe94f3a
Nikhil Popli <l6u2h3d9u4z5z9s4@truefoundryworkspace.slack.com>
2024-02-18 18:43:02 +0530

    add svc

---
+++ mnist-classifaction/deploy_model/deploy.py
@@ -0,0 +1,48 @@
+import argparse
+import logging
+from servicefoundry import (
+    Build,
+    PythonBuild,
+    Service,
+    Resources,
+    Port,
+    ArtifactsDownload,
+    TruefoundryArtifactSource,
+)
+
+logging.basicConfig(level=logging.INFO)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--workspace_fqn", type=str, required=True)
+parser.add_argument("--model_version_fqn", type=str, required=True)
+parser.add_argument("--host", type=str, required=True)
+parser.add_argument("--path", type=str, required=False)
+args = parser.parse_args()
+
+service = Service(
+    name="mnist-classification-svc",
+    image=Build(
+        build_spec=PythonBuild(
+            command="python gradio_demo.py",
+            requirements_path="requirements.txt",
+        )
+    ),
+    ports=[Port(port=8000, host=args.host, path=args.path)],
+    resources=Resources(
+        memory_limit=500,
+        memory_request=500,
+        ephemeral_storage_limit=600,
+        ephemeral_storage_request=600,
+        cpu_limit=0.3,
+        cpu_request=0.3,
+    ),
+    artifacts_download=ArtifactsDownload(
+        artifacts=[
+            TruefoundryArtifactSource(
+                artifact_version_fqn=args.model_version_fqn,
+                download_path_env_variable="MODEL_DOWNLOAD_PATH",
+            )
+        ]
+    ),
+)
+service.deploy(workspace_fqn=args.workspace_fqn)
---
+++ mnist-classifaction/deploy_model/predict.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+def load_model(model_path):
+    # Load the trained model
+    model = tf.keras.models.load_model(model_path)
+    return model
+
+def predict_fn(model, img_arr):
+  # Preprocess the image before passing it to the model
+  img_arr = tf.expand_dims(img_arr, 0)
+  img_arr = img_arr[:, :, :, 0]  # Keep only the first channel (grayscale)
+
+  # Make predictions
+  predictions = model.predict(img_arr)
+  predicted_label = tf.argmax(predictions[0]).numpy()
+
+  return str(predicted_label)
