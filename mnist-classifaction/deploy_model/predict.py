192b6d50fae4c3648e8a47b7c5bfe5c18ac20a77
Nikhil Popli <l6u2h3d9u4z5z9s4@truefoundryworkspace.slack.com>
2024-02-19 16:24:08 +0530

    formatting improve

--- mnist-classifaction/deploy_model/predict.py
+++ mnist-classifaction/deploy_model/predict.py
@@ -1,17 +1,20 @@
 import tensorflow as tf
+import numpy as np
 
-def load_model(model_path):
+
+def load_model(model_path: str):
     # Load the trained model
     model = tf.keras.models.load_model(model_path)
     return model
 
-def predict_fn(model, img_arr):
-  # Preprocess the image before passing it to the model
-  img_arr = tf.expand_dims(img_arr, 0)
-  img_arr = img_arr[:, :, :, 0]  # Keep only the first channel (grayscale)
 
-  # Make predictions
-  predictions = model.predict(img_arr)
-  predicted_label = tf.argmax(predictions[0]).numpy()
+def predict_fn(model, img_arr: np.ndarray):
+    # Preprocess the image before passing it to the model
+    img_arr = tf.expand_dims(img_arr, 0)
+    img_arr = img_arr[:, :, :, 0]  # Keep only the first channel (grayscale)
 
-  return str(predicted_label)
+    # Make predictions
+    predictions = model.predict(img_arr)
+    predicted_label = tf.argmax(predictions[0]).numpy()
+
+    return str(predicted_label)
