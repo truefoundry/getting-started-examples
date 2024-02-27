4279d853378bfe6354c702977c726f9c547a16e1
Maaz Karim <MaazKarim02@gmail.com>
2024-02-27 17:19:35 +0530

    fix: improve llm.py flow

--- llm-example/llm.py
+++ llm-example/llm.py
@@ -2,23 +2,31 @@ import os
 
 import requests
 
-SYSTEM_PROMPT = os.getenv("SYSTEM_PROMPT") or ""
+SYSTEM_PROMPT = """
+Given a natural language description of a flower's sepal and petal measurements as input, return a dictionary with the measurements. If a measurement is not mentioned, set its value to 0. The input sentence will contain descriptions of the sepal length, sepal width, petal length, and petal width.
+
+For example, given the input 'The flower has a sepal length of 5.1 cm and a petal width of 0.3 cm.', the function should return: { 'sepal_length': 5.1, 'sepal_width': 0, 'petal_length': 0, 'petal_width': 0.3 }
+
+Another example input could be 'This flower's sepal width is 3.5 cm and its petal length is 1.4 cm.', which should return: { 'sepal_length': 0, 'sepal_width': 3.5, 'petal_length': 1.4, 'petal_width': 0 }
+
+INPUT:
+"""
+
+TFY_LLM_GATEWAY_HOST = os.getenv("TFY_LLM_GATEWAY_HOST")
+TFY_API_KEY = os.getenv("TFY_API_KEY")
+MODEL_NAME = os.getenv("MODEL_NAME")
 
 
 def llm(prompt):
     response = requests.post(
-        os.getenv("TRUEFOUNDRY_API_ENDPOINT"),
+        TFY_LLM_GATEWAY_HOST,
         headers={
-            "Authorization": f"Bearer {os.getenv('TRUEFOUNDRY_API_KEY')}",
+            "Authorization": f"Bearer {TFY_API_KEY}",
         },
         json={
             "prompt": SYSTEM_PROMPT + prompt,
-            "model": os.getenv("TRUEFOUNDRY_MODEL"),
-            "temperature": 0.5,
+            "model": MODEL_NAME,
             "max_tokens": 200,
-            "top_p": 1.0,
-            "presence_penalty": 0.0,
-            "frequency_penalty": 0.0,
         },
     )
     data = response.json()
