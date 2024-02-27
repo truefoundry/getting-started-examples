b7b6ec3ab5216a67f03198bde0fe5c0928fdd9bb
Maaz Karim <MaazKarim02@gmail.com>
2024-02-28 03:53:29 +0530

    docs: Improve starter notebook
    - Modified app.py and deploy.py to contain artifacts
    - Add information for classification model in starter notebook
    
    Signed-off-by: Maaz Karim <MaazKarim02@gmail.com>

--- llm-example/app.py
+++ llm-example/app.py
@@ -6,7 +6,10 @@ import pandas as pd
 from fastapi import FastAPI
 from llm import llm
 
-model = joblib.load("iris_classifier.joblib")
+artifact_downloaded_path = os.environ.get("DTC_ARTIFACT")
+classifier = joblib.load(
+    artifact_downloaded_path + "/my-artifact/iris_classifier.joblib"
+)
 
 app = FastAPI(docs_url="/", root_path=os.getenv("TFY_SERVICE_ROOT_PATH", "/"))
 
@@ -18,5 +21,5 @@ def predict(prompt: str):
 
     response = llm(prompt=prompt)
     data = json.loads(response)
-    prediction = int(model.predict(pd.DataFrame([data]))[0])
-    return {"flower_name": IRIS_CLASSES[prediction]}
+    prediction = int(classifier.predict(pd.DataFrame([data]))[0])
+    return {"flower_name": IRIS_CLASSES[prediction], "input_data": data}
