2df6e99efd797d443d994b9ab38da23885a7a7a3
Maaz Karim <MaazKarim02@gmail.com>
2024-03-06 11:12:33 +0530

    fix: revise docker service usage

--- docker-deploy/deploy.py
+++ docker-deploy/deploy.py
@@ -1,41 +1,41 @@
 import argparse
 import logging
-from servicefoundry import Build, PythonBuild, Service, Resources, Port
+
+from servicefoundry import Build, DockerFileBuild, LocalSource, Port, Resources, Service
 
 logging.basicConfig(level=logging.INFO)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--workspace_fqn", required=True, type=str)
-parser.add_argument("--host", required=True, type=str)
+parser.add_argument("--name", required=True, type=str, help="Name of the application.")
+parser.add_argument(
+    "--workspace_fqn",
+    required=True,
+    type=str,
+    help="FQN of the workspace where application will be deployed.",
+)
+parser.add_argument(
+    "--host",
+    required=True,
+    type=str,
+    help="Host where the application will be available for access. Ex:- my-app.my-org.com",
+)
 args = parser.parse_args()
 
 image = Build(
-    build_spec=PythonBuild(
-        command="uvicorn app:app --port 8000 --host 0.0.0.0",
-        requirements_path="requirements.txt"
-    )
+    build_spec=DockerFileBuild(),
+    build_source=LocalSource(local_build=False),
 )
 
+
 service = Service(
-    name="ml-deploy",
+    name=args.name,
     image=image,
-    ports=[
-        Port(
-            port=8000,
-            host=args.host,
-        )
-    ],
+    ports=[Port(port=8080, host=args.host)],
     resources=Resources(
-      memory_limit=500,
-      memory_request=500,
-      ephemeral_storage_limit=600,
-      ephemeral_storage_request=600,
-      cpu_limit=0.3,
-      cpu_request=0.3
+        cpu_request=0.1,
+        cpu_limit=0.1,
+        memory_request=500,
+        memory_limit=500,
     ),
-    env={
-        "UVICORN_WEB_CONCURRENCY": "1",
-        "ENVIRONMENT": "dev"
-    }
 )
-service.deploy(workspace_fqn=args.workspace)
+service.deploy(workspace_fqn=args.workspace_fqn)
