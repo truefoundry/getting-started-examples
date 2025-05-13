ee24c126ad7a91cfdb22571b9acda78f2a3735d2
Bhavesh Patel <bhavesh@truefoundry.com>
2025-05-13 20:36:42 +0530

    test

--- deploy-ml-model/app.py
+++ deploy-ml-model/app.py
@@ -2,7 +2,11 @@ import os
 
 import joblib
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+import logging
 
 model = joblib.load("iris_classifier.joblib")
 app = FastAPI(docs_url="/", root_path=os.getenv("TFY_SERVICE_ROOT_PATH"))
@@ -18,3 +22,21 @@ def predict(sepal_length: float, sepal_width: float, petal_length: float, petal_
     )
     prediction = int(model.predict(pd.DataFrame([data]))[0])
     return {"prediction": prediction}
+
+@app.middleware("http")
+async def log_request(request: Request, call_next):
+    body = await request.body()
+    print(f"Request path: {request.url.path}")
+    print(f"Request method: {request.method}")
+    print(f"Request query params: {request.query_params}")
+    print(f"Request body: {body.decode()}")
+    response = await call_next(request)
+    return response
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    logging.error(f"Validation error for request {request.url}: {exc.errors()}")
+    return JSONResponse(
+        status_code=422,
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
