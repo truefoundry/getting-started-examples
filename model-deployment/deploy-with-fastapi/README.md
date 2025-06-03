f9af6df9ecd5f89d68ed4a519bb4870734196e52
Chirag Jain <jain.chirag925@gmail.com>
2025-06-03 12:00:50 +0530

    Add FastAPI

---
+++ model-deployment/deploy-with-fastapi/README.md
@@ -0,0 +1,4 @@
+Deploying ML Model with FastAPI
+---
+
+Docs Page: https://docs.truefoundry.com/docs/model-deployment/deploy-model-fastapi
---
+++ model-deployment/deploy-with-fastapi/train.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import joblib
+
+# Load data
+X, y = load_iris(return_X_y=True)
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+
+# Evaluate
+preds = clf.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, preds))
+
+# Save model
+joblib.dump(clf, "iris_model.joblib")
