be42642e05e507b05af6cedc00df6a9fcb213e17
Nikhil Popli <97437109+nikp1172@users.noreply.github.com>
2025-10-24 18:33:43 +0530

    Update __init__.py (#132)

--- banking-agent/src/agent/__init__.py
+++ banking-agent/src/agent/__init__.py
@@ -10,10 +10,10 @@ load_dotenv()
 # Initialize Traceloop
 TFY_API_KEY = os.environ.get("TFY_API_KEY")
 Traceloop.init(
-    api_endpoint=os.environ.get("TRACING_BASE_URL") || "",
+    api_endpoint=os.environ.get("TRACING_BASE_URL") or "",
     headers = {
         "Authorization": f"Bearer {TFY_API_KEY}",
-        "TFY-Tracing-Project": os.environ.get("TRACING_PROJECT_FQN") || "",
+        "TFY-Tracing-Project": os.environ.get("TRACING_PROJECT_FQN") or "",
     },
-    app_name = os.environ.get("TRACING_APPLICATION_NAME") || "",
+    app_name = os.environ.get("TRACING_APPLICATION_NAME") or "",
 )
