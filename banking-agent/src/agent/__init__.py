bbf5237bb4b6533ffe31a1a0b9eda078d1e7884a
Nikhil Popli <97437109+nikp1172@users.noreply.github.com>
2025-10-24 19:46:25 +0530

    update banking example (#133)

--- banking-agent/src/agent/__init__.py
+++ banking-agent/src/agent/__init__.py
@@ -9,11 +9,17 @@ load_dotenv()
 
 # Initialize Traceloop
 TFY_API_KEY = os.environ.get("TFY_API_KEY")
-Traceloop.init(
-    api_endpoint=os.environ.get("TRACING_BASE_URL") or "",
-    headers = {
-        "Authorization": f"Bearer {TFY_API_KEY}",
-        "TFY-Tracing-Project": os.environ.get("TRACING_PROJECT_FQN") or "",
-    },
-    app_name = os.environ.get("TRACING_APPLICATION_NAME") or "",
-)
+TRACING_PROJECT_FQN = os.environ.get("TRACING_PROJECT_FQN")
+TRACING_APPLICATION_NAME = os.environ.get("TRACING_APPLICATION_NAME")
+TRACING_BASE_URL = os.environ.get("TRACING_BASE_URL")
+
+if TRACING_PROJECT_FQN is not None and TRACING_APPLICATION_NAME is not None and TRACING_BASE_URL is not None:
+    Traceloop.init(
+        api_endpoint=TRACING_BASE_URL,
+        headers = {
+            "Authorization": f"Bearer {TFY_API_KEY}",
+            "TFY-Tracing-Project": TRACING_PROJECT_FQN,
+        },
+        app_name = TRACING_APPLICATION_NAME,
+    )
+
